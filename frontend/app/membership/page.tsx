2afff195a03e76386be1a03e7658e78c1a3daa07
MajaOlsen <majabacholsen@gmail.com>
2026-05-26 19:49:33 +0200

    membership start styling with no working accordion yet

--- frontend/app/membership/page.tsx
+++ frontend/app/membership/page.tsx
@@ -1,9 +1,22 @@
 "use client"
+import { IoIosArrowDown } from "react-icons/io";
+import SquareButton from "@/components/buttons/__SquareButton";
 
 export default function MembershipPage() {
   return (
     <main>
-      <h1>Medlemskab</h1>
+      <h1>Wash World</h1>
+      <h1 className="text-[var(--green-White-BG)] mt-[-10px]">medlemskaber</h1>
+      {/*membership v.1*/}
+      <div>
+        <div className="flex flex-col justify-center text-center gap-1 bg-[var(--grey-5)] p-6 shadow-[var(--box-Shadow)] mt-2">
+          <h2>Brilliant</h2>
+          <h5>Bedste vask året rundt</h5>
+          <div className="flex flex-row justify-center items-end gap-1"><h2>199</h2><h5>kr./md.</h5></div>
+          <SquareButton text="Tilføj medlemskab"/>
+          <div className="flex flex-row justify-center pt-2 text-[var(--green-White-BG)] items-center gap-1"><p>Se hvad den indeholder</p><IoIosArrowDown/></div>
+        </div>
+      </div>
     </main>
   );
 }
--- frontend/components/buttons/__SquareButton.tsx
+++ frontend/components/buttons/__SquareButton.tsx
@@ -1,1 +1,20 @@
+"use client"
 // Square button for the membership/payment section
+
+
+export default function SquareButton({ text, onClick }: ArrowButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        group relative flex justify-center gap-6 bg-[var(--green-White-BG)] text-white 
+        px-10 py-4 font-bold text-[16px] transition-all duration-300
+        hover:bg-[#00b058] shadow-[var(--box-Bottom-Green-Line)]
+      "
+    >
+    <span >{text}</span>
+      
+     
+    </button>
+  );
+}
