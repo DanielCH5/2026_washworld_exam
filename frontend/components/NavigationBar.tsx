c624d697f0fe659f758e3c6edf7330fd5e4f041a
MajaOlsen <majabacholsen@gmail.com>
2026-06-05 14:50:06 +0200

    navigation bar icons inserted and styled

--- frontend/components/NavigationBar.tsx
+++ frontend/components/NavigationBar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useAuth } from '@/context/AuthContext'
+import Image from "next/image";
 
 export default function NavigationBar() {
   const pathname = usePathname();
@@ -10,29 +11,37 @@ export default function NavigationBar() {
   if (!user) return null;
 
   const navItems = [
-    { label: "Cars", href: "/cars" },
-    { label: "Membership", href: "/membership" },
-    { label: "Map", href: "/" },
-    { label: "Help", href: "/help" },
-    { label: user.name, href: "/profile" }
+    { label: "Biler", href: "/cars", icon: "images/carsbutton.svg" },
+    { label: "Medlem", href: "/membership", icon: "images/membershipbutton.svg" },
+    { label: "", href: "/", icon: "images/homebutton.svg", featured: true },
+    { label: "Hjælp", href: "/help", icon: "images/helpbutton.svg" },
+    { label: user.name, href: "/profil", icon: "images/profilbutton.svg" },
   ];
 
   return (
-    <nav className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] bg-[var(--solid-Black)] px-4 py-2 z-50">
-      <div className="flex justify-around items-center">
+    <nav className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] bg-[var(--solid-Black)] px-4  z-50">
+      <div className="flex justify-around items-center py-2">
         {navItems.map((item) => (
           <Link
-            key={item.href}
-            href={item.href}
-            className={`flex flex-col items-center gap-1 no-underline
-              ${pathname === item.href ? "opacity-100" : "opacity-70"}
-            `}
+          key={item.href}
+          href={item.href}
+          className={`flex flex-col items-center gap-1 no-underline
+            ${pathname === item.href ? "opacity-100" : "opacity-70"}
+           ${item.featured ? "-translate-y-3" : ""}
+          `}
           >
-            <div className="w-8 h-8 bg-[var(--solid-White)] rounded-full" />
-            <span className="text-[var(--solid-White)]">
-              <p>{item.label}</p>
-            </span>
-          </Link>
+         <Image
+            src={item.icon}
+           alt={item.label}
+           width={item.featured ? 85 : 30}
+           height={item.featured ? 85 : 30}
+          />
+         {!item.featured && (
+           <span className="text-[var(--solid-White)]">
+             <p>{item.label}</p>
+           </span>
+         )}
+        </Link>
         ))}
       </div>
     </nav>
